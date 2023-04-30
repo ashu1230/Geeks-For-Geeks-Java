1c5653f00dc66c31c0ef645e53d5010725b538b9
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-05-01 00:15:20 +0530

    Update Subtree In N-ary Tree.java

--- Medium/Subtree In N-ary Tree.java
+++ Medium/Subtree In N-ary Tree.java
@@ -1,1 +1,23 @@
-.
+Given root of a n-ary tree find the number of duplicate subtree in the n-ary tree.
+Two trees are duplicate if they have the same structure with the same node values.
+
+Example 1:
+
+Input:
+1 N 2 2 3 N 4 N 4 4 3 N N N N N
+
+Output: 2
+Explanation: [4], [3] are duplicate subtree.
+Example 2:
+
+Input:
+1 N 2 3 N 4 5 6 N N N N
+
+Output: 0
+Explanation: No duplicate subtree found.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function duplicateSubtreeNaryTree() which takes root of n-ary tree as input, and return an integer value as number of duplicate subtrees.
+  
+  
+  
+  Quetion link:- https://practice.geeksforgeeks.org/problems/079dee7e7db7a784ed73f604e3cf1712432edf79/1
