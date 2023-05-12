b72e7a4efc09f5691538520b7cce21b0c0a55c25
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-05-12 17:11:17 +0530

    Update Array Operations.java
    DONE

--- Easy/Array Operations.java
+++ Easy/Array Operations.java
@@ -32,4 +32,25 @@ Constraints:
 1 <= n <= 10^5
 0 <= arr[i] <= 10^9
 #SOLUTION....
-  
+  class Solution {
+    public static int arrayOperations(int n, int[] arr) {
+        // code here
+        int res = 0; 
+        int cnt = 0; 
+        int zero = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0)
+                cnt++;
+            if (arr[i] == 0) {
+                zero= 1;
+                if (cnt > 0)
+                res++;
+                cnt = 0;
+            }
+        }
+        if (cnt > 0)
+            res++;
+        if (zero == 0)return -1;
+        return res;
+    }
+}
