8de2a5fa42018f1bf0e1515166fc79c40fd9438d
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-05-13 16:02:51 +0530

    Update Bit Magic.java

--- Easy/Bit Magic.java
+++ Easy/Bit Magic.java
@@ -41,5 +41,20 @@ Constraints:
   
   #SOULTION.......
   
-  
+  class Solution {
+    public static int bitMagic(int n, int[] arr) {
+        // code here
+        int left = 0;
+        int right = n-1;
+        int count = 0;
+        while(left < right){
+            count += arr[left] == arr[right] ? 0 : 1;
+            left++;
+            right--;
+        }
+        return (count+1)/2; // or return (int)Math.ceil((double)count/2);
+    }
+}
+        
+
   
