e9e7820996160f417d3e11250ddf812ba38bafe8
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-05-29 11:34:19 +0530

    Update CamelCase Pattern Matching.java

--- Medium/CamelCase Pattern Matching.java
+++ Medium/CamelCase Pattern Matching.java
@@ -45,3 +45,31 @@ S is the longest word in Dictionary.
   
   #SOLUTION......
   
+class Solution{
+    ArrayList<String> CamelCase(int N,String[] Dictionary,String Pattern){
+        //code here
+        ArrayList<String> res = new ArrayList<>();
+        for(int i=0;i<N;i++){
+            String s= Dictionary[i];
+            int idx=0;
+            for(int j=0;j<s.length();j++){
+                char c= s.charAt(j);
+                
+                if((int)c <=90){
+                    if(idx<Pattern.length() && c==Pattern.charAt(idx)) idx++;
+                    else break;
+                }
+            }
+            if(idx==Pattern.length()) res.add(s);
+        }
+        
+        if(res.size()<1) {
+            res.add("-1");
+            return res;
+        }
+        else{
+            Collections.sort(res);
+            return res;
+        }
+    }
+}
