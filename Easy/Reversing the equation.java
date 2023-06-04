1255ab12e194746e79a35ab7c78c09e758608260
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-06-04 23:27:14 +0530

    Update Reversing the equation.java

--- Easy/Reversing the equation.java
+++ Easy/Reversing the equation.java
@@ -24,3 +24,37 @@ Expected Auxiliary Space: O(|S|).
 Constraints:
 1<=|S|<=105
 The string contains only the characters '0' - '9', '+', '-', '*', and '/'.
+
+  //User function Template for Java
+class Solution{
+   
+    String reverseEqn(String S)
+    {
+        // your code here
+        // Stack<Character>st = new Stack();
+        StringBuilder s = new StringBuilder();
+        
+        for(int i = S.length() - 1; i >= 0 ; i--){
+            
+            if( i != 0 && (S.charAt(i) >= '0' && S.charAt(i) <= '9') && (S.charAt(i-1) >= '0' && S.charAt(i-1) <= '9') ){
+                continue;
+            }
+            else{
+                if(S.charAt(i) == '+' || S.charAt(i) == '-' || S.charAt(i) == '*' || S.charAt(i) == '/')
+                s.append(S.charAt(i));
+                else{
+                int j = i;
+                while(j < S.length() && S.charAt(j) >= '0' && S.charAt(j) <= '9')
+                {
+                   s.append(S.charAt(j));
+                   j++;
+                }
+                }
+            }
+            
+        }
+        
+        return s.toString();
+    }
+}
+
