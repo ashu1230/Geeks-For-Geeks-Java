65d008fdc3d81d6ae12ed02697d7b34cc7cc2fa4
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-06-11 17:25:39 +0530

    Update Adding Ones.java

--- Easy/Adding Ones.java
+++ Easy/Adding Ones.java
@@ -39,4 +39,27 @@ Constraints:
 1 ≤ updates[i] ≤ N, for all i from 1 to N.
   
   #SOLUTION....
-  
+  //User function Template for Java
+
+
+class Solution {
+
+    public static void update(int a[], int n, int updates[], int k)
+    {
+        // Your code goes here
+        for(int i=0;i<k;i++){
+            int ind=updates[i];
+            a[ind-1]++;
+        }
+        int last=0;
+        for(int i=0;i<n;i++){
+            if(a[i]!=0){
+                last+=a[i];
+                a[i]=last;
+            }else{
+                a[i]=last;
+            }
+        }
+    }
+    
+}
