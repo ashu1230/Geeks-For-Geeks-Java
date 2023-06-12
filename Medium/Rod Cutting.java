154576da9b72c9f1a4df28bf34ce4d29cc8e43ec
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-06-12 09:39:31 +0530

    Update Rod Cutting.java

--- Medium/Rod Cutting.java
+++ Medium/Rod Cutting.java
@@ -36,4 +36,25 @@ Constraints:
 
 #SOLUTION.......
   
-  
+  // Best Solution in JAVA
+
+public class Solution {
+    public int cutRod(int price[], int n) {
+        int[] dp = new int[n+1];
+        
+        // Base case
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            int maxVal = Integer.MIN_VALUE;
+            for (int j = 1; j <= i; j++) {
+                maxVal = Math.max(maxVal, price[j-1] + dp[i-j]);
+            }
+            dp[i] = maxVal;
+        }
+        
+        return dp[n];
+    }
+}
+
+
